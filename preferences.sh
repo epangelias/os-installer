bdf8ab7639fc1e4fc861b4ee007fe402c769a477
Epangelias <epangelias@gmail.com>
2024-08-23 15:54:33 -0700

    change pref

--- preferences.sh
+++ preferences.sh
@@ -4,3 +4,7 @@ gsettings set org.gnome.desktop.wm.preferences focus-mode 'sloppy'
 gsettings set org.gnome.desktop.background picture-uri "file://$(realpath ./black_pixel.png)"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$(realpath ./black_pixel.png)"
 gsettings set org.gnome.mutter workspaces-only-on-primary false
+
+git config --global user.email "epangelias@gmail.com"
+git config --global user.name "Epangelias"
+git config --global credential.helper store
