887c35ad3ed295a5ac46ac2d5b37dd4c81fb526f
Epangelias <epangelias@gmail.com>
2024-08-23 16:24:22 -0700

    move black pixel photo

--- preferences.sh
+++ preferences.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+cp -r ./black_pixel.png ~/Pictures/Wallpapers/black_pixel.png
 gsettings set org.gnome.desktop.wm.preferences focus-mode 'sloppy'
-gsettings set org.gnome.desktop.background picture-uri "file://$(realpath ./black_pixel.png)"
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$(realpath ./black_pixel.png)"
+gsettings set org.gnome.desktop.background picture-uri "file://$(realpath ~/Pictures/Wallpapers/black_pixel.png)"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://$(realpath ~/Pictures/Wallpapers/black_pixel.png)"
 gsettings set org.gnome.mutter workspaces-only-on-primary false
 
 git config --global user.email "epangelias@gmail.com"
